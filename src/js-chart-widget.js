c0df8195f1f240bc2d0d114c2e9df0e013f264ca
EvaLok <squidstock@hotmail.com>
2016-05-18 17:17:21 -0600

    basic demo using static data

---
+++ src/js-chart-widget.js
@@ -0,0 +1,54 @@
+(function(){
+    var _chart;
+
+    init();
+
+    function init(){
+        if( typeof Chart !== 'object' ) {
+            var script_tag = document.createElement('script');
+            script_tag.setAttribute("type","text/javascript");
+            script_tag.setAttribute("src",
+                "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.3/Chart.js");
+
+            // non-standard browsers
+            if (script_tag.readyState) {
+                script_tag.onreadystatechange = function () { // For old versions of IE
+                    if (this.readyState == 'complete' || this.readyState == 'loaded') {
+                        main();
+                    }
+                };
+            } else { // standard browsers
+                script_tag.onload = main;
+            }
+            // Try to find the head, otherwise default to the documentElement
+            (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
+        } else {
+            main();
+        }
+    }
+
+    function main(){
+        _chart = Chart;
+
+        var ctx = document.getElementById("demoChart1");
+        var chart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3]
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+})();
