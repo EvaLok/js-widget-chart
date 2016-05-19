35c5e59ecb98f58646a3af229c4d5a4789ffff3e
EvaLok <squidstock@hotmail.com>
2016-05-18 18:21:18 -0600

    working off of dummy data

--- src/js-chart-widget.js
+++ src/js-chart-widget.js
@@ -4,9 +4,8 @@
     );
     var _chart;
 
-    var dataLocation = '/fake-data-source.js';
+    var dataLocation = 'fake-data-source.json';
     var data = {};
-    var dataVar = '';
 
     init();
 
@@ -23,18 +22,31 @@
 
         var elements = document.getElementsByClassName("js-chart-widget");
         var elementsLength = elements.length;
+        var ids = [];
+
+        for( var i = 0; i < elementsLength; i++ ){
+            ids.push(elements[i].getAttribute('data-identifier'));
+        }
+
+        loadData(ids, function(){
+            renderCharts(elements);
+        });
+    }
+
+    function renderCharts( elements ){
+        var elementsLength = elements.length;
 
         for( var i = 0; i < elementsLength; i++ ){
             new Chart(elements[i], {
                 type: 'line',
                 data: {
-                    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                    labels: data[elements[i].getAttribute('data-identifier')].labels,
                     datasets: [{
                         label: (
                             '# of Votes for element '
                             + elements[i].getAttribute('data-identifier')
                         ),
-                        data: [12, 19, 3, 5, 2, 3]
+                        data: data[elements[i].getAttribute('data-identifier')].points
                     }]
                 },
                 options: {
@@ -48,16 +60,6 @@
                 }
             });
         }
-    }
-
-
-
-    function fetchData(){
-
-    }
-
-    function renderCharts(){
-
     }
 
     function loadScript(script, callback){
@@ -81,5 +83,24 @@
             document.getElementsByTagName("head")[0]
             || document.documentElement
         ).appendChild(script_tag);
+    }
+
+    function loadData(ids, callback){
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', dataLocation, true);
+        xobj.onreadystatechange = function() {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                var json = JSON.parse(xobj.responseText);
+                for( var k in json ){
+                    data[k] = json[k];
+                }
+
+                console.log(data);
+
+                callback();
+            }
+        };
+        xobj.send(null);
     }
 })();
